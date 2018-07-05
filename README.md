1e05ff99b4b985924cc0f05643a5bf2387efa43d
Ira Miller <public@iramiller.com>
2018-07-05 16:54:20 -0500

    add and remove

--- README.md
+++ README.md
@@ -1,68 +1,32 @@
-# git-config
+# guld-git-remote-cli
 
-Guld configuration helper manages git config files.
+Manage git remotes the guld way.
 
 ### Example Output
-
-```
-$ guld-config list
-user.username=isysd
-user.signingkey=C7EA0E59D0660BF6848614B6441BDDD420F44729
-commit.gpgsign=true
-alias.pushall=!git remote | xargs -L1 -I R git push R $2
-host.github=isysd
-host.bitbucket=isysd
-host.gitlab=isysd
-core.testing4=true
-core.repositoryformatversion=0
-core.filemode=true
-core.bare=false
-core.logallrefupdates=true
-```
 
 ### Install
 
 ```
-npm i -g guld-config
+npm i -g guld-git-remote-cli
 ```
 
 ### Usage
 
-```
-// async
-// assume it's empty
-var cfg = await getConfig('local') // {}
-cfg.core.test = true
-setConfig('cfg.core.test', true)
-var cfg = await getConfig('local') // {core: {test: true}}
-```
-
-##### Node
-
-```
-const { guldName, getConfig, setConfig, unsetConfig } = require('guld-config')
-```
-
 ##### CLI
 
 ```
-  Usage: guld-config [options] [command]
+  Usage: guld-git-remote [options] [command]
+
+  Manage git remotes the guld way.
 
   Options:
 
-    -V, --version             output the version number
-    --global                  Use the global config file
-    --local                   Use the local config file
-    --system                  Use the system config file.
-    -f, --file <config-file>  Use the given config file.
-    -h, --help                output usage information
+    -V, --version         output the version number
+    -h, --help            output usage information
 
   Commands:
 
-    name                      Get the guld name of the current user.
-    get <key>                 Get a config by key.
-    set <key> <value>         Set a config key to the given value.
-    unset                     Get the distro, if linux OS.
-    list                      List all config key/value pairs.
+    remove|delete [name]  Remove one or all remotes from the given or current directory.
+    add [options] [slug]  Add one or all user branch remotes to the current directory.
 ```
 
--- cli.js
+++ cli.js
@@ -1,116 +1,35 @@
 #!/usr/bin/env node
 // eslint-disable-file no-console
-const { guldName, getConfig, setConfig, unsetConfig } = require('./index.js')
-const flat = require('flat')
-const _get = require('lodash.get')
 const program = require('commander')
 const VERSION = require('./package.json').version
+const { deleteRemote, addRemote } = require('guld-git-remote')
 
+/* eslint-disable no-console */
 program
-  // .usage('<key> Get a config by key (alias of get)')
-  // .usage('<key> <value> Set a config key to the given value (alias of set)')
+  .name('guld-git-remote')
   .version(VERSION)
-  .option('--global', 'Use the global config file')
-  .option('--local', 'Use the local config file')
-  .option('--system', 'Use the system config file.')
-  .option('-f, --file <config-file>', 'Use the given config file.')
+  .description('Manage git remotes the guld way.')
+  // .option('-q, --quiet', '')
 program
-  .command('name')
-  .description('Get the guld name of the current user.')
+  .command('remove [name]')
+  .alias('delete')
+  .description('Remove one or all remotes from the given or current directory.')
+  .action(async (rname, options) => {
+    await deleteRemote(rname)
+    console.log('ok')
+  })
 program
-  .command('get <key>')
-  .description('Get a config by key.')
-program
-  .command('set <key> <value>')
-  .description('Set a config key to the given value.')
-program
-  .command('unset')
-  .description('Get the distro, if linux OS.')
-program
-  .command('list')
-  .description('List all config key/value pairs.')
+  .command('add [slug]')
+  .description('Add one or all user branch remotes to the current directory.')
+  .option('-u --user <user>', 'Add remotes for the given user. (default to all local branch)', '*')
+  .option('-h --host <host>', 'Add remotes only for the given host.')
+  .option('-p --protocol <protocol>', 'Use the given protocol for urls.', 'ssh')
+  .action(async (slug, options) => {
+    var user
+    if (options.user && options.user !== '*') user = options.user
+    await addRemote(user, slug, options.host, options.protocol)
+    console.log('ok')
+  })
 
 program.parse(process.argv)
-
-var scope
-if (program.global) scope = 'global'
-else if (program.system) scope = 'system'
-else if (program.local) scope = 'local'
-else if (program.file) scope = program.file
-else scope = 'merged'
-var cmd
-if (program.args.length === 0) cmd = 'name'
-else if (program.commands.map(c => c._name).indexOf(program.args[0]) !== -1) cmd = program.args.shift()
-/* eslint-disable no-console */
-switch (cmd) {
-  case 'name':
-    guldName().then(console.log)
-    break
-  case 'get':
-    getConfig(scope).then(c => {
-      var val = _get(c, program.args[0])
-      if (val) console.log(val)
-    })
-    break
-  case 'set':
-    setConfig(program.args[0], program.args[1], scope).then(console.log)
-    break
-  case 'unset':
-    unsetConfig(program.args[0], scope)
-    break
-  case 'list':
-    getConfig(scope).then(conf => {
-      var cfg = flat(conf)
-      for (var c in cfg) {
-        console.log(`${c}=${cfg[c]}`)
-      }
-    })
-    break
-  /* case 'setup':
-    inquirer
-      .prompt([
-        {
-          name: guldname,
-          type: input,
-          message: "What is your individual guld name?",
-          default: await guldName()
-        },
-        {
-          name: fullname,
-          type: input,
-          message: "What is your public full name? (optional)",
-          default: await guldName()
-        },
-        {
-          name: guldmail,
-          type: input,
-          message: "What is your public email address? (optional)"
-        },
-        {
-          name: signingkey,
-          type: input,
-          message: "Which of these is your PGP key id? (blank for group)"
-        }
-      ])
-      .then(answers => {
-        answers.commit = {gpgsign: true}
-        setupConfig(answers).then(c => {
-          var cfg = flat(c)
-          for (var c in cfg) {
-            console.log(`${c}=${cfg[c]}`)
-          }
-        })
-      })
-    break */
-  default:
-    if (program.args.length === 1) {
-      getConfig(scope).then(c => {
-        var val = _get(c, program.args[0])
-        if (val) console.log(val)
-      })
-    } else if (program.args.length === 2) {
-      setConfig(program.args[0], program.args[1], scope).then(console.log)
-    }
-    break
-}
-/* eslint-enable no-console */
+if (program.rawArgs.length === 2) program.help()
